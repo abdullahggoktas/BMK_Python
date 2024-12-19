534b3fd11ee2c699b84b10f1284e2027a4152067
Abdullah Göktaş <abdullahggoktas@icloud.com>
2024-12-19 21:32:46 +0300

    New files added.

---
+++ 23) class.py
@@ -0,0 +1,18 @@
+# Class
+class Product:
+    pass
+    # method
+    # property 
+
+
+
+# Instance, Object
+p1 = Product()
+p2 = Product()
+
+# sonuc = type(p1)
+# sonuc = type(p2)
+
+# sonuc = p1
+
+print(p2)
